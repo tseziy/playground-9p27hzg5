535aae697ecc15d24a3bd2f4cd73d2a3a04f3f1c
platonov <platonov@skbkontur.ru>
2022-11-11 22:56:58 +0500

    полный путь

--- battle1-project/test_universe.py
+++ battle1-project/test_universe.py
@@ -1,6 +1,6 @@
 import subprocess
 
-respone = subprocess.check_output("py universe.py").decode('utf-8').rstrip()
+respone = subprocess.check_output("py /project/target/universe.py").decode('utf-8').rstrip()
 if respone == '1':
     print('TECHIO> success true')
 else:
