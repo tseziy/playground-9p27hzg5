48cfb216e22e4e984f8a931dec743093509a6e4f
platonov <platonov@skbkontur.ru>
2022-11-12 13:27:33 +0500

    11

--- battle1-project/test_universe.py
+++ battle1-project/test_universe.py
@@ -1,9 +1,5 @@
-import subprocess
-import os
 from universe import main
 
-#print(os.path.abspath(os.curdir))
-#respone = subprocess.check_output("python3 universe.py").decode('utf-8').rstrip()
 respone = main()
 if respone == '1':
     print('TECHIO> success true')
