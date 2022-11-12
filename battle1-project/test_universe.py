6c4efef2f25fb08ec6a5772a56b15a84e1b2ad8a
platonov <platonov@skbkontur.ru>
2022-11-12 13:27:23 +0500

    111

--- battle1-project/test_universe.py
+++ battle1-project/test_universe.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
+from universe import main
 
-print(os.path.abspath(os.curdir))
-respone = subprocess.check_output("python3 universe.py").decode('utf-8').rstrip()
+#print(os.path.abspath(os.curdir))
+#respone = subprocess.check_output("python3 universe.py").decode('utf-8').rstrip()
+respone = main()
 if respone == '1':
     print('TECHIO> success true')
 else:
--- battle1-project/universe.py
+++ battle1-project/universe.py
@@ -1,7 +1,3 @@
-import sys
-import math
-
-# Write your code here.
-# Click on "RUN" to execute it
-result = None
-print(result)
+def main():
+    result = 1
+    return result
